d2af3167dc0e7d92a095c655cba78597a85f2dd2
Aichaz <el.mardizarrouk@gmail.com>
2025-01-30 14:28:54 +0100

    uppp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import HomePage from './pages/Home/Homepage';
+import Nav from './Nav/Nav'
+import Footer from './Nav/Footer';
 function App() {
+  const location = useLocation(); 
   return (
     <div className="App">
-        <HomePage/>
+      {location.pathname !== '/' && <Nav />}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+      {location.pathname !== '/' && <Footer/>}
+      
     </div>
   );
 }
--- src/Nav/Nav.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './Nav.css'
-export default function Nav() {
-  return (
-    <div>
-     
-    </div>
-  )
-}
